914fd22faba9d4c23fe27be661554438ed0d2a41
Celio Grand <celiogrand@outlook.com>
2025-03-05 18:45:23 +0800

    hello world

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "unary";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              rust-bin.beta.latest.default
+            ];
+          };
+      }
+    );
+}
